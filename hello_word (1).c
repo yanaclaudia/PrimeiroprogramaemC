e3c4d168936104b344ff015f9062730744c1a658
yanaclaudia <142348798+yanaclaudia@users.noreply.github.com>
2023-08-15 16:37:02 -0300

    Exercício inicial de programação (Algoritmos)

---
+++ hello_word (1).c
@@ -0,0 +1,13 @@
+#include <stdio.h> // utilizar função printf
+
+//arquivo função principal do progrma
+//todo progrma em c deve ter esta função 
+int main(void)
+{
+  //exibe uma mensagem em tela
+  printf("Meu primeiro programa em C!\n\n");
+  printf("Disciplina de Lógica e Programação\n\n");
+  printf("Engenharia de Controle e Automação\n\n");
+  printf("Aluna:Ana Claudia");
+  return 0;
+}
